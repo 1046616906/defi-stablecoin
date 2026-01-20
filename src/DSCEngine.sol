07bff1fea0ad5cb13392bda237934ead5c93f8b0
root <root@DESKTOP-KTDHBRD.localdomain>
2026-01-20 11:28:26 +0800

    feat: to better understand add comments

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -31,7 +31,7 @@ contract DSCEngine is Validations, ReentrancyGuard {
     uint256 private constant PRECISION = 1e18; // 精确度
     uint256 private constant LIQUIDATION_THRESHOLD = 50; //清算阈值
     uint256 private constant LIQUIDATION_PRECISION = 100; // 计算精度
-    uint256 private constant MIN_HEALTH_FACTOR = 1e18; // 计算精度
+    uint256 private constant MIN_HEALTH_FACTOR = 1e18; // 最低阈值
 
     mapping(address token => address priceFeeds) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256 amount)) s_userAddressDeposit;
@@ -43,9 +43,17 @@ contract DSCEngine is Validations, ReentrancyGuard {
 
     //  function =============================================
 
-    event DepositCollateral(address indexed userAddress, address indexed tokenAddress, uint256 indexed amount);
+    event DepositCollateral(
+        address indexed userAddress,
+        address indexed tokenAddress,
+        uint256 indexed amount
+    );
 
-    constructor(address[] memory tokenAddress, address[] memory priceFeedsAddress, address dscAddress) {
+    constructor(
+        address[] memory tokenAddress,
+        address[] memory priceFeedsAddress,
+        address dscAddress
+    ) {
         if (tokenAddress.length != priceFeedsAddress.length) {
             revert DSCEngine__TokenAddressesAndpriceFeedAddressesMustBeSameLength();
         }
@@ -67,7 +75,10 @@ contract DSCEngine is Validations, ReentrancyGuard {
     function depositCollateralAndMintDsc() external {}
 
     /* 只存入抵押品 不铸造 DSC ，因为铸造会影响 健康值  health factor */
-    function depositCollateral(address tokenCollateralAddress, uint256 tokenAmount)
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 tokenAmount
+    )
         external
         moreThanZero(tokenAmount)
         isAllowedToken(tokenCollateralAddress)
@@ -75,7 +86,11 @@ contract DSCEngine is Validations, ReentrancyGuard {
     {
         s_userAddressDeposit[msg.sender][tokenCollateralAddress] += tokenAmount;
         emit DepositCollateral(msg.sender, tokenCollateralAddress, tokenAmount);
-        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), tokenAmount);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(
+            msg.sender,
+            address(this),
+            tokenAmount
+        );
         if (!success) revert DSCEngine__TransFerFaile();
     }
 
@@ -86,7 +101,9 @@ contract DSCEngine is Validations, ReentrancyGuard {
      * 2. 需要判断铸造人是否可以制造amountDscToMint的DSC，不能 mintAmount > collateral
      * 3. 需要判断健康值是否达标，不达标都应revert
      */
-    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+    function mintDsc(
+        uint256 amountDscToMint
+    ) external moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
         bool success = i_dsc.mint(msg.sender, amountDscToMint);
@@ -114,7 +131,9 @@ contract DSCEngine is Validations, ReentrancyGuard {
      * @return totalDscMinted  已经铸造DSC的数量
      * @return collateralValInUsd  抵押物品的 USDT 值
      */
-    function _getAccountInformation(address user)
+    function _getAccountInformation(
+        address user
+    )
         internal
         view
         returns (uint256 totalDscMinted, uint256 collateralValInUsd)
@@ -123,18 +142,34 @@ contract DSCEngine is Validations, ReentrancyGuard {
         collateralValInUsd = getAccountCollateralValue(user);
     }
 
+    /**
+     *
+     * @param user 用户地址
+     * 想判断出用户的健康值，需要用户 已经铸造的DSC的总数（totalDscMinted） 和  抵押货币的总价值
+     * 公式  collateralValInUsd * 清算阈值  / totalDscMinted   1000 * 0.5 / 100  = 5； 5>1
+     */
     function _healthFactor(address user) internal view returns (uint256) {
-        (uint256 totalDscMinted, uint256 collateralValInUsd) = _getAccountInformation(user);
+        (
+            uint256 totalDscMinted,
+            uint256 collateralValInUsd
+        ) = _getAccountInformation(user);
         /**
          * collateralAdjustedForThreshold 可借出的数量 也可以称之为 打折的价格
          * 2000 * 50 / 100  ===== 2000 / 2 = 1000
          * collateralAdjustedForThreshold = 1000   totalDscMinted = 100
          * 1000 * 1e18 / 100  =  10 * 1e19
          */
-        uint256 collateralAdjustedForThreshold = (collateralValInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-        return collateralAdjustedForThreshold * PRECISION / totalDscMinted;
+        uint256 collateralAdjustedForThreshold = (collateralValInUsd *
+            LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
     }
 
+    /**
+     *
+     * @param user  用户地址
+     * 1. 获取用户的健康值
+     * 2. 判断健康值 是否大于 最低的阈值
+     */
     function _revertIfHealthFactorIsBroken(address user) internal view {
         uint256 userHealthFactor = _healthFactor(user);
         if (userHealthFactor < MIN_HEALTH_FACTOR) {
@@ -144,7 +179,16 @@ contract DSCEngine is Validations, ReentrancyGuard {
 
     //  view  pure =======================================================
     /* 获取账号下抵押物品的数量 */
-    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
+    /**
+     *
+     * @param user 用户的地址
+     * 1. constructor 中 创建一个包含所有 token 的 Array
+     * 2. 遍历数组，获取用户存入该token的数量
+     * 3. 通过token 和 数量 获取用户的抵押品的总金额
+     */
+    function getAccountCollateralValue(
+        address user
+    ) public view returns (uint256 totalCollateralValueInUsd) {
         for (uint256 i = 0; i < s_collarteralToken.length; i++) {
             address token = s_collarteralToken[i];
             uint256 amount = s_userAddressDeposit[user][token];
@@ -152,8 +196,18 @@ contract DSCEngine is Validations, ReentrancyGuard {
         }
     }
 
-    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
-        (, int256 price,,,) = AggregatorV3Interface(token).latestRoundData();
+    /**
+     *
+     * @param token 代币地址
+     * @param amount 代币数量
+     * 使用AggregatorV3Interface获取每个货币的价格来计算传入的token总价值
+     * @notice  最后因为AggregatorV3Interface返回的 精度是 1e8 因为要同步精度，所有要 * 1e10 ，至于除以 1e18(PRECISION) ，可以当作为了把amount 转换成个数，比如 1个eth = 1e18 ， / 1e18(PRECISION) 为了把amount变为我们认为的个数也就是 “ 1 ”
+     */
+    function getUsdValue(
+        address token,
+        uint256 amount
+    ) public view returns (uint256) {
+        (, int256 price, , , ) = AggregatorV3Interface(token).latestRoundData();
         return (uint256(price) * 1e10 * amount) / PRECISION;
     }
 }
